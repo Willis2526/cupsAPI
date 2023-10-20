2c5eeb4abee9732f81ca8496bf2354fec28a7172
Willis2526 <=>
2023-10-20 14:34:44 -0500

    Add CORS middleware

--- cupsapi/app.py
+++ cupsapi/app.py
@@ -1,6 +1,7 @@
 """ The main app """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from cupsapi.routers.default import DefaultRouter
 
@@ -12,6 +13,14 @@ app = FastAPI(
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     }
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Include the routes
